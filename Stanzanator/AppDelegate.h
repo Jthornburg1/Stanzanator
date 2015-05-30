e06341290110a6aafaf4f71d0992e50483cfaeef
jonathan thornburg <reliable2192@gmail.com>
2015-05-30 09:58:47 -0600

    Initial Commit

---
+++ Stanzanator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stanzanator
+//
+//  Created by jonathan thornburg on 5/30/15.
+//  Copyright (c) 2015 jonathan thornburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
