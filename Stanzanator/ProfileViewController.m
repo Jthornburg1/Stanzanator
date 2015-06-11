40b20d9d511ad033c0b0063a113d5255dff1a4f9
jonathan thornburg <reliable2192@gmail.com>
2015-06-10 21:36:54 -0600

    Saves poems to PoemsToReadVC. Saves username, age, location, and photo to ProfileViewController...which is now green with yellow tableView cells (if they ever populate).

--- Stanzanator/ProfileViewController.m
+++ Stanzanator/ProfileViewController.m
@@ -8,26 +8,115 @@
 
 #import "ProfileViewController.h"
 
-@interface ProfileViewController ()<UITableViewDataSource, UITableViewDelegate>
+@interface ProfileViewController ()<UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UIImagePickerControllerDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *nameText;
+@property (weak, nonatomic) IBOutlet UITextField *ageText;
+@property (weak, nonatomic) IBOutlet UITextField *locationText;
 @property (weak, nonatomic) IBOutlet UIImageView *image;
-@property (strong, nonatomic) IBOutlet UIView *tableView;
-@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
-@property (weak, nonatomic) IBOutlet UILabel *ageLabel;
-@property (weak, nonatomic) IBOutlet UILabel *homeTownLabel;
-@property (weak, nonatomic) IBOutlet UILabel *favLabel;
-@property (weak, nonatomic) IBOutlet UILabel *leastFavLabel;
-@property (weak, nonatomic) IBOutlet UILabel *passWord;
-
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIButton *editButton;
+@property (nonatomic, readwrite) BOOL inEditingMode;
 @end
 
 @implementation ProfileViewController
-- (IBAction)editProfileButtonTapped:(id)sender {
-}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.inEditingMode = NO;
+    
+    self.nameText.text = [PFUser currentUser].username;
+    
 }
+- (void)viewWillAppear:(BOOL)animated
+{
+    PFUser *user = [PFUser currentUser];
+    
+    self.ageText.text = user[@"age"];
+    self.locationText.text = user[@"location"];
+    PFFile *photoFile = user[@"photo"];
+    [photoFile getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.image.image = [UIImage imageWithData:data];
+        });
+    }];
+}
+
+
+- (IBAction)editProfileButtonTapped:(id)sender {
+    
+    self.inEditingMode = !self.inEditingMode;
+    
+    self.ageText.borderStyle = UITextBorderStyleRoundedRect;
+    self.ageText.userInteractionEnabled = YES;
+    
+    self.locationText.borderStyle = UITextBorderStyleRoundedRect;
+    self.locationText.userInteractionEnabled = YES;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    PFUser *user = [PFUser currentUser];
+    if (textField.tag == 1) {
+        user[@"age"] = textField.text;
+        textField.borderStyle = UITextBorderStyleNone;
+        textField.userInteractionEnabled = NO;
+        [user saveInBackground];
+    }else{
+        user[@"location"] = textField.text;
+        textField.borderStyle = UITextBorderStyleNone;
+        textField.userInteractionEnabled = NO;
+        [user saveInBackground];
+    }
+    
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    PFUser *user = [PFUser currentUser];
+    if (textField.tag == 1) {
+        user[@"age"] = textField.text;
+        textField.borderStyle = UITextBorderStyleNone;
+        textField.userInteractionEnabled = NO;
+        [user saveInBackground];
+
+    }else{
+        user[@"location"] = textField.text;
+        textField.borderStyle = UITextBorderStyleNone;
+        textField.userInteractionEnabled = NO;
+        [user saveInBackground];
+    }
+    return YES;
+}
+
+- (IBAction)uploadPhotoButtonTapped:(id)sender {
+    
+    UIImagePickerController *picker = [UIImagePickerController new];
+    
+    picker.delegate = self;
+    
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo
+{
+    PFUser *user = [PFUser currentUser];
+    
+    self.image.image = image;
+    
+    user[@"photo"] = [PFFile fileWithData:UIImageJPEGRepresentation(self.image.image, 0.95)];
+    
+    [user saveInBackground];
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+# pragma mark - TableView delegate methods
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [UITableViewCell new];
