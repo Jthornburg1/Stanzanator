fda52a2df8d4fcb55c9a4de5d833b5f7e04e9563
jonathan thornburg <reliable2192@gmail.com>
2015-06-23 21:20:31 -0600

    Now it's a Make Private switch

--- Stanzanator/StanzaViewController.m
+++ Stanzanator/StanzaViewController.m
@@ -11,17 +11,17 @@
 
 @interface StanzaViewController ()<UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *titleTextField;
+@property (weak, nonatomic) IBOutlet UISwitch *privateSwitch;
 @property (weak, nonatomic) IBOutlet UITextView *poemBodyText;
 @property (strong, nonatomic) Poem *poem;
 @property (nonatomic) BOOL privateFileOnly;
-@property (weak, nonatomic) IBOutlet UIButton *privateButton;
 
 @end
 
 @implementation StanzaViewController
 - (IBAction)visitTheWebforIdeas:(id)sender {
     // Links to yahoo.com: that string will be an index of an array of strings eventually
-    NSArray *URLArray = @[@"https//anncoulter.com",@"https//eonlone.com",@"https//thugkitchen.com",@"https//http://www.openbible.info/topics/m",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//"];
+    NSArray *URLArray = @[@"https//anncoulter.com",@"https//eonline.com",@"https//thugkitchen.com",@"https//http://www.openbible.info/topics/m",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//",@"https//"];
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.yahoo.com"]];
     // Needs to return the user to StanzaViewController
 }
@@ -40,6 +40,9 @@
     NSArray *titleArray = @[@"Breakfast at Midnight", @"Title2", @"How to Raise Your Children",@"Just a Little Innocent Fun",@"She Presents Herself", @"He Presents Himself"];
     self.titleTextField.text = titleArray[rand];
 }
+- (IBAction)makePrivateSwitched:(id)sender {
+    self.privateFileOnly = !self.privateFileOnly;
+}
 - (IBAction)doneButtonTapped:(id)sender {
     [[PoemController sharedInstance]addPoemWithTitle:self.titleTextField.text bodyText:self.poemBodyText.text date:[NSDate date] private:self.privateFileOnly];
 
@@ -47,14 +50,8 @@
     
 }
 
-- (IBAction)makePrivateButtonTapped:(id)sender {
-   
-    
-    self.privateFileOnly = !self.privateFileOnly;
-    if (self.privateFileOnly) {
-        [self.privateButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
-    }
-}
+
+
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
