9c7cc6e893e9110878f39c12b8d060c7629376e7
jonathan thornburg <reliable2192@gmail.com>
2015-07-04 14:46:17 -0600

    contains words about sonnets and limericks.

--- Stanzanator/LimerickViewController.m
+++ Stanzanator/LimerickViewController.m
@@ -18,7 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    self.textView.text = @"Everyone knows the the bouncy limerick:\n\nblah BLAH blah blah BLAH blah blah BLAH\nblah BLAH blah blah BLAH blah blah BLAH\nblah BLAH blah blah BLAH\nblah BLAH blah blah BLAH\nblah BLAH blah blah BLAH blah blah BLAH\n\n";
+    self.textView.text = @"Everyone knows the bouncy limerick. It may be the cleverest of all poetic forms. It is but a meter and rhyme structure and could apply to any theme but usually defaults to humor, mischief, smut, slander and the like. Limericks generally, but not necessarily veer away from drama, seriousness or even sincerity. Most will recognize the layout:\n\nblah BLAH blah blah BLAH blah blah BLAH\nblah BLAH blah blah BLAH blah blah BLAH\nblah BLAH blah blah BLAH\nblah BLAH blah blah BLAH\nblah BLAH blah blah BLAH blah blah BLAH\n\nOh!..the last words of lines 1, 2, and 5 ought to rhyme as well as the last words of lines 3 and 4!\n\nWriting limericks can help build poetic chops and likely yield some real zingers!\n\n";
 
     
 }
